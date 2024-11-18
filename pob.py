d740923c271a1526afb46a2d9af846f25982a8f7
Luis Pinto <luchopinto100@gmail.com>
2024-11-18 14:18:46 -0600

    Update pob.py

--- pob.py
+++ pob.py
@@ -96,5 +96,46 @@ with div3:
 with div4:
     barchart4
     
+#to create Tabs:    
+tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9 = st.tabs(["Map", "Poblacion", "PBI", "Empleo", "Agricultura", "Mineria", "Pesca", "Turismo", "Social"  ])
+with tab1: #Map
+    map=px.scatter_mapbox(df_selection1, lat=df_selection1['Lat'], lon=df_selection1['Lon'], zoom=8, color=df_selection1['DISTRITO'], 
+                 size=df_selection1['Total'], width=1200, height=800, title="Mapa Distrital")
+    map.update_layout(mapbox_style="open-street-map")
+    map.update_layout(margin={"r":0, "t":100, "l":0, "b":10})
+    #map.show()
+    st.plotly_chart(map, use_container_width=True)
+
+with tab3: #PBI
+    peru_pbi = pd.read_excel('pbi_peru.xlsx', sheet_name='cuadro4', usecols='A:R', header=6)
+    peru_pbi1 = peru_pbi.melt(id_vars='DEPARTAMENTO', value_vars=['2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', 
+               '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023'], var_name='Year', value_name='PBI')
+    peru_pbi1_df =peru_pbi1.sort_values(by=('PBI'), ascending=False)
+
+    st.sidebar.header("Para filtrar PBI por departamento, seleccione:" )
+    departamento=st.sidebar.multiselect("Departamento:", 
+                                    options=peru_pbi1_df["DEPARTAMENTO"].unique())
+   
+    df_selection2 = peru_pbi1.query("DEPARTAMENTO == @departamento")
+
+    st.text("PBI por Departamento.  Valores a Precios Corrientes (Miles de soles). Fuente: INEI.")
+    #st.dataframe(peru_pbi1)
+    #st.dataframe(df_selection2.loc[:,["DEPARTAMENTO", "Year", "PBI"]])
+
+    #GRAPH
+    #undepartamento_pbi=df_selection2.groupby('DEPARTAMENTO')['PBI'].sum()
+    undepartamento_pbi=pd.DataFrame (df_selection2.loc[:,["DEPARTAMENTO", "Year", "PBI"]])
+    chart_pbi=px.line(undepartamento_pbi, x='Year', y=['PBI'])
+    chart_pbi.update_layout(title= 'PBI Departamental Anual -  Valores a Precios Corrientes (Miles de soles). Fuente: INEI.', 
+                            width=800, height=400,  yaxis_title='PBI- miles de Soles', xaxis_title='Año') 
+    chart_pbi
+
+    chart2_pbi=px.line(peru_pbi1, x='Year', y='PBI', color='DEPARTAMENTO', template='ggplot2', facet_col= 'DEPARTAMENTO', facet_col_wrap=6, title='pbi DEPARTAMENTAL', markers=True)
+    chart2_pbi.update_layout(title= 'PBI Anual por Departamento -  Valores a Precios Corrientes (Miles de soles).', 
+                            width=1600, height=2000)
+    chart2_pbi.update_yaxes (matches=None)
+   
+    
+    chart2_pbi
 
 st.caption("***Baul-Analytics - 2024***")
